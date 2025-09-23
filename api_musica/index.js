dd2a099922530d1522d304b31b24ce7f48a562db
GABY <gabrielykardec@gmail.com>
2025-09-23 12:58:08 +0200

    Subo el index.js editado con la parte de los endpoints de artista actualizado

--- api_musica/index.js
+++ api_musica/index.js
@@ -1,31 +1,54 @@
 
-const express = require('express');
+//Configuración inicial
+const express = require('express'); //libreria que hemos instalado
+const fs = require('fs').promises;
 const app = express();
-app.use(express.json());
+app.use(express.json()); //nuestra variable app del tipo express va a utilizar json
 //Leer los txt (nuestra base de datos)
-const dataPath = path.join(__dirname, 'data');
-
-async function leerAlbumes() {
-    try {
-        const data = await fs.readFile(path.join(dataPath, 'albumes.json'), 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error leyendo albumes:', error);
-        return [];
-    }
+async function leerArtistas(){
+    await fs.access('artistas.json')
+    const data = await fs.readFile('artistas.json', 'utf8');
+    const artistas = JSON.parse(data);
+    return artistas;
 }
 
-async function leerArtistas() {
-    try {
-        const data = await fs.readFile(path.join(dataPath, 'artistas.json'), 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('Error leyendo artistas:', error);
-        return [];
-    }
+async function guardarArtistas(artistas){
+    await fs.access('artistas.json');
+    const data = JSON.stringify(artistas, null, 2);
+    await fs.writeFile('artistas.json', data, 'utf8');
+}
+async function leerAlbumes(){
+    await fs.access('albumes.json')
+    const data = await fs.readFile('albumes.json', 'utf8');
+    const albumes = JSON.parse(data);
+    return albumes;
+}
+
+async function guardarAlbumes(albumes){
+    await fs.access('albumes.json');
+    const data = JSON.stringify(albumes, null, 2);
+    await fs.writeFile('albumes.json', data, 'utf8');
 }
 //donde se configuran los endpoints (la API en si)
+    //ALBUMES
 
+    //ARTISTAS
+    app.post('/api/artista', async (req, res) => {
+        const {nombre_completo, año_nacimiento } = req.body; //esta info nos la tienen que pasar en Body
+        //crear nuevo artista
+        artistas.push({nombre_completo,año_nacimiento});
+        await guardarArtistas(artistas);
+    });
+    app.delete('/api/artista/:nombre_completo', async (req, res) => {
+        const {nombre_completo} = req.params;
+        const artistas = await leerArtistas();
+        //buscar índice del artista
+        const artista_index = artistas.findIndex(a => a.nombre_completo.toLowerCase === nombre_completo.toLowerCase());
+        //eliminar el artista que contenga ese indice buscado
+        const artista_eliminado = artistas.splice(artista_index, 1)[0];
+        await guardarArtistas(artistas);
+    
+    });
 
 //establecer los puertos por los que accederemos
 const port = process.env.port || 80;
